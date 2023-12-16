e9ce06562b26010c1bbcb34b7d296d3926799b47
Pierre1231 <70148312+Pierre1231@users.noreply.github.com>
2023-12-17 00:08:30 +0800

    🐞 fix: fix title error

--- NN_SGD.py
+++ NN_SGD.py
@@ -2,8 +2,8 @@
 # -*- encoding: utf-8 -*-
 """
 Author: HUANG Jingyi
-File: network3.py
-Date: 2023/11/13 17:51
+File: 
+Date: 2023/11/13 
 Email: pierrehuang1998@gmail.com
 Description:
 """
@@ -16,25 +16,26 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
+
 class NN:
-    def __init__(self, weight_init='random', activation='relu'):
+    def __init__(self, weight_init="random", activation="relu"):
         np.random.seed(0)
-        if weight_init == 'random':
+        if weight_init == "random":
             self.w1 = np.random.randn(2, 30)
             self.w2 = np.random.randn(30, 3)
-        elif weight_init == 'zeros':
+        elif weight_init == "zeros":
             self.w1 = np.zeros((2, 30))
             self.w2 = np.zeros((30, 3))
-        elif weight_init == 'ones':
+        elif weight_init == "ones":
             self.w1 = np.ones((2, 30))
             self.w2 = np.ones((30, 3))
 
         # 选择激活函数
-        if activation == 'relu':
+        if activation == "relu":
             self.activation = self.relu
-        elif activation == 'sigmoid':
+        elif activation == "sigmoid":
             self.activation = self.sigmoid
-        elif activation == 'tanh':
+        elif activation == "tanh":
             self.activation = self.tanh
 
     def relu(self, x):
@@ -59,7 +60,7 @@ class NN:
 
         return self.s
 
-    def fit(self, data, y, learning_rate=1e-3, epochs=1, method='fixed', batch_size=32):
+    def fit(self, data, y, learning_rate=1e-3, epochs=1, method="fixed", batch_size=32):
         loss = np.ones(epochs) * 100
         epoch = 0
         initial_learning_rate = learning_rate
@@ -68,8 +69,8 @@ class NN:
         while min(loss) > 10 and epoch < epochs:
             # Split data into mini-batches based on batch_size
             for i in range(0, data_arr, batch_size):
-                x_train1 = data[i:i + batch_size]
-                y_train1 = y[i:i + batch_size]
+                x_train1 = data[i : i + batch_size]
+                y_train1 = y[i : i + batch_size]
 
                 y_pred = self.call(x_train1)
                 delta = 1e-7
@@ -82,9 +83,9 @@ class NN:
                 dz1 = (1 - self.hiden_layer1) * self.hiden_layer1 * da1
                 dw1 = x_train1.T.dot(dz1)
 
-                if method == 'variable':
+                if method == "variable":
                     # Update learning rate
-                    learning_rate = initial_learning_rate * (1 - epoch*10 / epochs)
+                    learning_rate = initial_learning_rate * (1 - epoch * 10 / epochs)
                     print("Learning rate:", learning_rate)
 
                 self.w1 -= learning_rate * dw1
@@ -95,8 +96,6 @@ class NN:
         loss = loss[0:epoch]
         print("Epoch:", epoch - 1)
         return loss
-
-
 
 
 def read_data_x(name):
@@ -113,6 +112,7 @@ def read_data_x(name):
     x2 = np.expand_dims(x2, axis=1)
     return np.concatenate((x1, x2), axis=1)
 
+
 def read_data_y(name):
     y = []
     file = open(name)
@@ -120,6 +120,7 @@ def read_data_y(name):
         y.append(int(line[0]))
     y = np.array(y)
     return y
+
 
 def labelToVec(y):
     y3 = []
@@ -132,14 +133,15 @@ def labelToVec(y):
             y3.append([0, 0, 1])
     return y3
 
+
 def plot_y(x_test, out):
-    y1 = out[:,0]
-    y2 = out[:,1]
-    y3 = out[:,2]
-    ax = plt.axes(projection='3d')
-    xline = x_test[:,0]
-    yline = x_test[:,1]
-    ax.scatter3D(xline, yline, y3, c='r', marker='o')
+    y1 = out[:, 0]
+    y2 = out[:, 1]
+    y3 = out[:, 2]
+    ax = plt.axes(projection="3d")
+    xline = x_test[:, 0]
+    yline = x_test[:, 1]
+    ax.scatter3D(xline, yline, y3, c="r", marker="o")
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Output")
@@ -150,31 +152,42 @@ def visualize_neurons(nn, x_test):
     # Visualize neurons with activation greater than zero (ai,k > 0)
     active_neurons = np.where(nn.hidden_layer_activations > 0)
     # plt.scatter(x_test[:, 0], x_test[:, 1], c='b', marker='o', label='Data')
-    plt.scatter(x_test[active_neurons, 0], x_test[active_neurons, 1], c='r', marker='o', label='Active Neurons (ai,k > 0)')
+    plt.scatter(
+        x_test[active_neurons, 0],
+        x_test[active_neurons, 1],
+        c="r",
+        marker="o",
+        label="Active Neurons (ai,k > 0)",
+    )
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.legend()
     plt.title("Visualization of Active Neurons")
     plt.show()
 
+
 def main():
     x = read_data_x("data_x.txt")
     y = read_data_y("data_y.txt")
     y3 = labelToVec(y)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y3, test_size=0.2, random_state=37)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y3, test_size=0.2, random_state=37
+    )
 
     # 可以在这里选择初始值策略
-    weight_init = 'random'  # 可以选择 'random'、'zeros'、'ones' 或其他支持的初始值策略
-    activation = 'sigmoid'  # 可以选择 'relu'、'sigmoid'、'tanh' 或其他支持的激活函数
+    weight_init = "random"  # 可以选择 'random'、'zeros'、'ones' 或其他支持的初始值策略
+    activation = "sigmoid"  # 可以选择 'relu'、'sigmoid'、'tanh' 或其他支持的激活函数
     mymodel = NN(weight_init=weight_init, activation=activation)
     epochs = 10000
     learning_rate = 1e-4
 
-    method = 'fixed'  # 'fixed' or 'variable'
+    method = "fixed"  # 'fixed' or 'variable'
     batch_size = 32
 
-    loss = mymodel.fit(x_train, y_train, learning_rate, epochs, method=method, batch_size=batch_size)
+    loss = mymodel.fit(
+        x_train, y_train, learning_rate, epochs, method=method, batch_size=batch_size
+    )
     out = mymodel.call(x_test)
 
     out3 = np.argmax(out, axis=1)
@@ -186,12 +199,13 @@ def main():
 
     # Plot the log loss against epochs
     plt.figure()
-    plt.plot(np.log(loss), label='Log Loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Log Loss')
-    plt.title('Log Loss vs. Epochs')
+    plt.plot(np.log(loss), label="Log Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Log Loss")
+    plt.title("Log Loss vs. Epochs")
     plt.legend()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
--- NN_add_layer.py
+++ NN_add_layer.py
@@ -2,8 +2,8 @@
 # -*- encoding: utf-8 -*-
 """
 Author: HUANG Jingyi
-File: network3.py
-Date: 2023/11/13 17:51
+File: 
+Date: 2023/11/13 
 Email: pierrehuang1998@gmail.com
 Description:
 """
@@ -16,24 +16,25 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
+
 class NN:
-    def __init__(self, weight_init='random', activation='relu'):
+    def __init__(self, weight_init="random", activation="relu"):
         np.random.seed(0)
-        if weight_init == 'random':
+        if weight_init == "random":
             self.w1 = np.random.randn(2, 30)
             self.w2 = np.random.randn(30, 30)
             self.w3 = np.random.randn(30, 30)
             self.w4 = np.random.randn(30, 30)
             self.w5 = np.random.randn(30, 30)
             self.w6 = np.random.randn(30, 3)
-        elif weight_init == 'zeros':
+        elif weight_init == "zeros":
             self.w1 = np.zeros((2, 30))
             self.w2 = np.zeros((30, 30))
             self.w3 = np.zeros((30, 30))
             self.w4 = np.zeros((30, 30))
             self.w5 = np.zeros((30, 30))
             self.w6 = np.zeros((30, 3))
-        elif weight_init == 'ones':
+        elif weight_init == "ones":
             self.w1 = np.ones((2, 30))
             self.w2 = np.ones((30, 30))
             self.w3 = np.ones((30, 30))
@@ -42,11 +43,11 @@ class NN:
             self.w6 = np.ones((30, 3))
 
         # 选择激活函数
-        if activation == 'relu':
+        if activation == "relu":
             self.activation = self.relu
-        elif activation == 'sigmoid':
+        elif activation == "sigmoid":
             self.activation = self.sigmoid
-        elif activation == 'tanh':
+        elif activation == "tanh":
             self.activation = self.tanh
 
     def relu(self, x):
@@ -79,7 +80,7 @@ class NN:
 
         return self.s
 
-    def fit(self, data, y, learning_rate=1e-3, epochs=1, method='fixed', batch_size=32):
+    def fit(self, data, y, learning_rate=1e-3, epochs=1, method="fixed", batch_size=32):
         loss = np.ones(epochs) * 100
         epoch = 0
         initial_learning_rate = learning_rate
@@ -88,8 +89,8 @@ class NN:
         while min(loss) > 10 and epoch < epochs:
             # Split data into mini-batches based on batch_size
             for i in range(0, data_arr, batch_size):
-                x_train1 = data[i:i + batch_size]
-                y_train1 = y[i:i + batch_size]
+                x_train1 = data[i : i + batch_size]
+                y_train1 = y[i : i + batch_size]
 
                 y_pred = self.call(x_train1)
                 delta = 1e-7
@@ -114,7 +115,7 @@ class NN:
                 dz1 = (1 - self.hidden_layer1) * self.hidden_layer1 * da1
                 dw1 = x_train1.T.dot(dz1)
 
-                if method == 'variable':
+                if method == "variable":
                     # Update learning rate
                     learning_rate = initial_learning_rate * (1 - epoch / epochs)
 
@@ -131,6 +132,7 @@ class NN:
         print("Epoch:", epoch - 1)
         return loss
 
+
 def read_data_x(name):
     x1 = []
     x2 = []
@@ -145,6 +147,7 @@ def read_data_x(name):
     x2 = np.expand_dims(x2, axis=1)
     return np.concatenate((x1, x2), axis=1)
 
+
 def read_data_y(name):
     y = []
     file = open(name)
@@ -152,6 +155,7 @@ def read_data_y(name):
         y.append(int(line[0]))
     y = np.array(y)
     return y
+
 
 def labelToVec(y):
     y3 = []
@@ -164,48 +168,61 @@ def labelToVec(y):
             y3.append([0, 0, 1])
     return y3
 
+
 def plot_y(x_test, out):
-    y1 = out[:,0]
-    y2 = out[:,1]
-    y3 = out[:,2]
-    ax = plt.axes(projection='3d')
-    xline = x_test[:,0]
-    yline = x_test[:,1]
-    ax.scatter3D(xline, yline, y3, c='r', marker='o')
+    y1 = out[:, 0]
+    y2 = out[:, 1]
+    y3 = out[:, 2]
+    ax = plt.axes(projection="3d")
+    xline = x_test[:, 0]
+    yline = x_test[:, 1]
+    ax.scatter3D(xline, yline, y3, c="r", marker="o")
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Output")
     plt.show()
 
+
 def visualize_neurons(nn, x_test):
     # Visualize neurons with activation greater than zero (ai,k > 0)
     active_neurons = np.where(nn.hidden_layer_activations1 > 0)
-    plt.scatter(x_test[:, 0], x_test[:, 1], c='b', marker='o', label='Data')
-    plt.scatter(x_test[active_neurons, 0], x_test[active_neurons, 1], c='r', marker='o', label='Active Neurons (ai,k > 0)')
+    plt.scatter(x_test[:, 0], x_test[:, 1], c="b", marker="o", label="Data")
+    plt.scatter(
+        x_test[active_neurons, 0],
+        x_test[active_neurons, 1],
+        c="r",
+        marker="o",
+        label="Active Neurons (ai,k > 0)",
+    )
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.legend()
     plt.title("Visualization of Active Neurons")
     plt.show()
 
+
 def main():
     x = read_data_x("data_x.txt")
     y = read_data_y("data_y.txt")
     y3 = labelToVec(y)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y3, test_size=0.2, random_state=37)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y3, test_size=0.2, random_state=37
+    )
 
     # 可以在这里选择初始值策略
-    weight_init = 'random'  # 可以选择 'random'、'zeros'、'ones' 或其他支持的初始值策略
-    activation = 'sigmoid'  # 可以选择 'relu'、'sigmoid'、'tanh' 或其他支持的激活函数
+    weight_init = "random"  # 可以选择 'random'、'zeros'、'ones' 或其他支持的初始值策略
+    activation = "sigmoid"  # 可以选择 'relu'、'sigmoid'、'tanh' 或其他支持的激活函数
     mymodel = NN(weight_init=weight_init, activation=activation)
     epochs = 10000
     learning_rate = 1e-4
 
-    method = 'fixed'  # 'fixed' or 'variable'
+    method = "fixed"  # 'fixed' or 'variable'
     batch_size = 32
 
-    loss = mymodel.fit(x_train, y_train, learning_rate, epochs, method=method, batch_size=batch_size)
+    loss = mymodel.fit(
+        x_train, y_train, learning_rate, epochs, method=method, batch_size=batch_size
+    )
     out = mymodel.call(x_test)
 
     out3 = np.argmax(out, axis=1)
@@ -213,6 +230,7 @@ def main():
     print("Accuracy:", accuracy_score(out3, y_test3))
     visualize_neurons(mymodel, x_test)
     plot_y(x_test, out)
+
 
 if __name__ == "__main__":
     main()
